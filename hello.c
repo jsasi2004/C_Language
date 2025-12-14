02cc11f13ffd955fa64af453bae60e934db17a59
Sashank Jonnadula <jsasi2804@gmail.com>
2025-12-14 18:38:59 +0000

    learned primitive datatypes

--- hello.c
+++ hello.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
-
-int main() {
-  // Run using gcc -Wall -save-temps hello.c -o hello
-  printf("hello world");
-  return 0;
+#include <stdbool.h>
+int main(){
+    int a = 1 ;
+    float b = 1.2 ;
+    char c = 'c' ;
+    double d = 2.3 ;
+    _Bool e = 0 ;
+    bool f = false ;
+    printf("int a = %i\n",a);
+    printf("float b = %.1f\n",b);
+    printf("char c = %c\n",c);
+    printf("double d = %lf\n",d);
+    printf("_Bool e = %d\n",e);
+    printf("bool f = %d\n",f);
+    return 0;
 }
