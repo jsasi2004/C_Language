785bf54b28bd206e19fa21ea625096daed91d6d2
Sashank Jonnadula <jsasi2804@gmail.com>
2025-12-14 18:39:52 +0000

    learned sizeof and %zu for size_t

--- hello.c
+++ hello.c
@@ -3,15 +3,15 @@
 int main(){
     int a = 1 ;
     float b = 1.2 ;
-    char c = 'c' ;
+    char c = 96 ;
     double d = 2.3 ;
     _Bool e = 0 ;
     bool f = false ;
-    printf("int a = %i\n",a);
-    printf("float b = %.1f\n",b);
-    printf("char c = %c\n",c);
-    printf("double d = %lf\n",d);
-    printf("_Bool e = %d\n",e);
-    printf("bool f = %d\n",f);
+    printf("size of int a = %zu\n",sizeof(a));
+    printf("size of float b = %zu\n",sizeof(b));
+    printf("size of char c = %zu\n",sizeof(c));
+    printf("size of double d = %zu\n",sizeof(d));
+    printf("size of _Bool e = %zu\n",sizeof(e));
+    printf("size of bool f = %zu\n",sizeof(f));
     return 0;
 }
