c3293f45ad636b207d2aa854a6f832c933f883a7
Sashank Jonnadula <jsasi2804@gmail.com>
2025-12-15 10:19:24 +0000

    Type Casting

--- hello.c
+++ hello.c
@@ -1,33 +1,10 @@
 #include <stdio.h>
-#include <stdbool.h>
+#include <stdlib.h>
 int main(){
-    int normalInt = 1000;                       // standard int 
-    double normalDouble = 3.14;                 // standard double
-
-    short int small = -100;                     // smaller int
-    unsigned int count = 25;                    // only positive int
-    long int big = 1234567890;                  // larger int
-    long long int veryBig = 9223372036854775807; // very large int
-    unsigned long long int huge = 18446744073709551615U; // very large, only positive
-    long double precise = 3.141592653589793238L; // extended precision
-
-    printf("Normal int: %d\n", normalInt);
-    printf("Normal double: %lf\n", normalDouble);
-    printf("Small: %hd\n", small);
-    printf("Count: %u\n", count);
-    printf("Big: %ld\n", big);
-    printf("Very Big: %lld\n", veryBig);
-    printf("Huge: %llu\n", huge);
-    printf("Precise: %Lf\n\n", precise);
-
-    printf("Size of int: %zu bytes\n", sizeof(int));
-    printf("Size of double: %zu bytes\n", sizeof(double));
-    printf("Size of short int: %zu bytes\n", sizeof(short int));
-    printf("Size of unsigned int: %zu bytes\n", sizeof(unsigned int));
-    printf("Size of long int: %zu bytes\n", sizeof(long int));
-    printf("Size of long long int: %zu bytes\n", sizeof(long long int));
-    printf("Size of unsigned long long int: %zu bytes\n", sizeof(unsigned long long int));
-    printf("Size of long double: %zu bytes\n", sizeof(long double));
-
+    float result = (float)5/2;
+    printf("result is %.0f\n",result);
+    char nums[]="1234";
+    int mynum = atoi(nums);
+    printf("%i\n",mynum+1);
     return 0;
 }
