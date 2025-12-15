245089929ef38c342802b001afbf48c1fbaf1622
Sashank Jonnadula <jsasi2804@gmail.com>
2025-12-15 11:19:58 +0000

    learned inputs

--- hello.c
+++ hello.c
@@ -1,10 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 int main(){
-    float result = (float)5/2;
-    printf("result is %.0f\n",result);
-    char nums[]="1234";
-    int mynum = atoi(nums);
-    printf("%i\n",mynum+1);
+    int a;
+    scanf("%i",&a);
+    printf("%i\n",a);
+
+    char name[30];
+    scanf("%s",name);
+    printf("Hello %s!\n",name);
+    int c;
+    while((c = getchar())!='\n' && c!=EOF){
+
+    }
+    char fullName[30];
+    fgets(fullName,sizeof(fullName),stdin);
+    printf("My name is: %s",fullName);
+    printf("%zu\n",sizeof(fullName));
+
+
+    char c0;
+    printf("Enter a character: ");
+    c0=getchar();
+    printf("%c\n",c0);
+
+    puts("Hello");
+    putchar(c0);
+while((c = getchar())!='\n' && c!=EOF){
+
+    }
+    char temp[30];
+    printf("Enter your name: ");
+    fgets(temp,sizeof(temp),stdin);
+    puts(temp);
     return 0;
 }
